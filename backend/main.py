61e2f1e524e716fca9f064b12c9ffa151ca49300
Aaron <aaronpromail@gmail.com>
2024-12-05 15:38:02 +0100

    add chatbot response from server

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
+import rag_chatbot
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+)
+
+
+@app.post("/ask-bot")
+async def chatbot(message=Body(embed=True)):
+    retrieval_chain = rag_chatbot.retrieval_chain
+    response = retrieval_chain.invoke({"input": message})
+    bot_answer = response["answer"]
+    return bot_answer
